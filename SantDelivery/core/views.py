af5507ff30c445c79295e28b13e749409450bb43
Balf <fabalf2@gmail.com>
2021-06-29 22:35:36 -0400

    cambios

--- SantDelivery/core/views.py
+++ SantDelivery/core/views.py
@@ -7,27 +7,6 @@ from .models import Proveedores
 def index(request):
     return render(request, 'core/index.html')
 
-def dulce(request):
-    return render(request, 'core/dulce.html')
-
-def salada(request):
-    return render(request, 'core/salada.html')
-
-def mixta(request):
-    return render(request, 'core/mixta.html')
-
-def contacto(request):
-    return render(request, 'core/contacto.html')
-
-def nosotros(request):
-    return render(request, 'core/nosotros.html')
-
-def sismos(request):
-    return render(request, 'core/sismos.html')
-
-def home(request):
-    return redirect(to="index")
-
 def ingreso_proveedor(request):
     datos = {
         'form' : ProveedoresForm()
