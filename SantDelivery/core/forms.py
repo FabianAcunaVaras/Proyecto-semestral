165b0e4b0f368177bdd8396f2d1a9da9558650a3
Cristian Alberto Tapia Tamayo <cri.tapiat@duocuc.cl>
2021-06-30 00:10:50 -0400

    migraciones

--- SantDelivery/core/forms.py
+++ SantDelivery/core/forms.py
@@ -1,16 +1,1 @@
 from django.forms import ModelForm
-from .models import Proveedores, Pais
-
-class ProveedoresForm(ModelForm):
-
-    class Meta:
-        model = Proveedores
-        fields =['idProveedor', 'Nombre', 'Fono', 'Direccion' ,'Mail', 'Pass' ,'IdPais']
-
-class PaisForm(ModelForm):
-
-    class Meta:
-        model = Pais
-        fields = ['idPais', 'nombrePais']
-
-        
