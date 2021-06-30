b263eca6b20cbefe16142fe01a7301ad11e3b968
raurojas <rau.rojas@duocuc.cl>
2021-06-30 02:19:46 -0400

    modificacion

--- SantDelivery/core/urls.py
+++ SantDelivery/core/urls.py
@@ -4,9 +4,11 @@ from .views import index, Comida , Contacto , Despacho , Sobre_nosotros
 
 urlpatterns = [
     path('', index, name="index"),
+    path('Sobre_nosotros', Sobre_nosotros, name="Sobre_nosotros"),
     path('Comida', Comida, name="Comida"),
-    path('Contacto', Contacto, name="Contacto"),
     path('Despacho', Despacho, name="Despacho"),
-    path('Sobre_nosotros', Sobre_nosotros, name="Sobre_nosotros")
+    path('Contacto', Contacto, name="Contacto"),
+    
+    
 
 ]
